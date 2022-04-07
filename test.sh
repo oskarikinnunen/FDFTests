81531bcda8dc40191ed4ff269404adeff45cba76
Oskari Kinnunen <okinnune@c2r6p3.hive.fi>
2022-04-07 18:14:56 +0300

    commit

--- test.sh
+++ test.sh
@@ -1,11 +1,36 @@
-FDFPATH=~/Projects/FDF #CHANGE THIS TO THE EVALUATEES PATH
+FDFPATH=~/FDF #CHANGE THIS TO THE EVALUATEES PATH
 
 FDF=$FDFPATH/FDF
 mypath=$(pwd)
 
-
-for filename in $mypath/input/*; do
-	file=$(basename "$filename")
-	echo "FILE FOUND UNDER INPUT $file"
-	#$fillit $filename > "$mypath/their_res/$file"
+echo "SUMMARY" > $mypath/output/summary
+for filepath in $mypath/input/*; do
+	filename=$(basename "$filepath")
+	echo "FILE FOUND UNDER INPUT $filename"
+	echo "FILE PATH $filepath"
+	cd $FDFPATH
+	
+	#staging
+	mkdir $mypath/output/$filename
+	rm -rf $mypath/output/$filename/*
+	#run program and wake up the terminal with applescript
+	osascript $mypath/applescript.applescript
+	./FDF $filepath > $mypath/output/$filename/out 1>&1 &
+	lastpid=$(echo "$!")
+	#leaks output
+	sleep 1
+	leaks $lastpid > $mypath/output/$filename/leaks1
+	echo "$filename" >> $mypath/output/summary
+	leaks $lastpid | grep "leaks for\|malloc" >> $mypath/output/summary
+	sleep 1
+	leaks $lastpid > $mypath/output/$filename/leaks2
+	leaks $lastpid | grep "leaks for\|malloc" >> $mypath/output/summary
+	#kill the process
+	kill $lastpid
+	#osascript applescript.SCPT $filepath
+	#"tell application "Terminal"
+	#	set currentTab to do script (\"cd FDF && ./FDF \"$filepath\" input/42\")
+	#	activate
+	#end tell"
+	#$fillit $filepath > "$mypath/their_res/$filename"
 done
