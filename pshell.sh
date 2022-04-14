34cd83ec1d393a543404b1da3785d65e9bbf252f
Oskari Kinnunen <eino.oskari.kinnunen@gmail.com>
2022-04-14 07:52:05 +0300

    modified pshell

--- pshell.sh
+++ pshell.sh
@@ -1,5 +1,5 @@
 #filecontent=$(cat testfile)
-FDF="/home/oskari/FDFREPO"
+FDF="/home/oskari/Projects/FDF"
 mypath=$(pwd)
 
 #timeout 1s ./FDF $mypath/$1 &>> $mypath/outputs.log
@@ -9,8 +9,14 @@ cd $FDF
 timeout 1s ./FDF $FILEPATH &> /dev/null
 #./FDF $FILEPATH &> /dev/null
 if [ $? -ne 0 ]; then
-	timeout 1s ./FDF $FILEPATH &>> $FILEPATH
-	mv $FILEPATH $mypath/fails/perm_$2_FAIL
+	echo "Exitcode: $?" >> $FILEPATH
+	stupid=$(cat $FILEPATH | grep "Exitcode: 0")
+	if [ stupid != "" ]; then
+		rm $FILEPATH
+	else
+		timeout 1s ./FDF $FILEPATH &>> $FILEPATH
+		mv $FILEPATH $mypath/fails/perm_$2_FAIL
+	fi
 else
 	rm -rf $FILEPATH
 fi
